d491514e2834beeea41bc063a46b6d7455cbb325
alves45 <alvesdelima.lucas45@gmail.com>
2022-07-04 12:05:06 -0300

    Adicionado componente image, que devolve um elemento html img com a imagem em conjunto no formato base64

---
+++ src/components/image.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/css";
+import fs from "fs";
+import path from "path";
+import component from "../tools/component.js";
+
+export default class extends component {
+  constructor(document) {
+    super(document);
+    this.builder(this);
+  }
+  render(pathImage) {
+    let image = this.document.createElement("img");
+    image.src =
+      "data:image/" +
+      (path.basename(pathImage).match(/(?<=\.)[^\.]+$/g) || "") +
+      ";base64, " +
+      fs.readFileSync(pathImage).toString("base64");
+    return image;
+  }
+}
