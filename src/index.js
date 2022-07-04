b5126e83864b17f4d65e7d0708345bb737bf97b3
alves45 <alvesdelima.lucas45@gmail.com>
2022-07-03 22:59:29 -0300

    Modificado para renderizar todos os curriculos localizados na pasta ./src/curricula/ em formato de pdf e html na pasta de ./curriculaBuilt/

--- src/index.js
+++ src/index.js
@@ -1,41 +1,59 @@
 import { promises as fs } from "fs";
 import puppeteer from "puppeteer";
 import inlineCss from "inline-css";
-import { curriculumVitae } from "./curriculumVitae.js";
 import devServer from "../dev/server.js";
+import path from "path";
 
-let curriculum = curriculumVitae();
-
-const fileName = "curriculumVitae";
-console.log(process.env.NODE_ENV);
-
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+async function generatePdf(html, pathToFile) {
+  return new Promise(async (resolve) => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    const htmlWithCssInline = await inlineCss(html, { url: "/" });
+    await page.setContent(htmlWithCssInline, {
+      waitUntil: "networkidle0", //await completely load html
+    });
+    await page.pdf({
+      path: pathToFile,
+      format: "A4",
+      printBackground: true,
+      pageRanges: "1-1",
+    });
+    await browser.close();
+    resolve();
   });
-  const page = await browser.newPage();
-  const htmlWithCssInline = await inlineCss(
-    curriculum.documentElement.innerHTML,
-    { url: "/" }
-  );
-  await page.setContent(htmlWithCssInline, {
-    waitUntil: "networkidle0", //await completely load html
-  });
-  await page.pdf({
-    path: fileName + ".pdf",
-    format: "A4",
-    printBackground: true,
-    pageRanges: "1-1",
-  });
-  await browser.close();
-  console.log("pdf loaded");
-})();
-
-fs.writeFile(fileName + ".html", curriculum.documentElement.innerHTML).then(
-  () => console.log("html loaded")
-);
-
-if (process.env.NODE_ENV === "development") {
-  devServer(curriculum);
 }
+
+const PATH_CURRICULA = "./src/curricula/";
+const PATH_BUILDS = "./curriculaBuilt/";
+
+async function buildCurricula() {
+  let curriculaFiles = await fs.readdir(PATH_CURRICULA);
+  return Promise.all(
+    await curriculaFiles.map(async (curriculumFile) => {
+      return new Promise(async (resolve) => {
+        function buildPathExtension(extension) {
+          return path.resolve(
+            PATH_BUILDS,
+            curriculumFile.replace("js", extension)
+          );
+        }
+        let curriculumPath = path.resolve(PATH_CURRICULA, curriculumFile);
+        let htmlModule = new (await import(curriculumPath)).default().render();
+        await fs.writeFile(buildPathExtension("html"), htmlModule);
+        await generatePdf(htmlModule, buildPathExtension("pdf"));
+        console.log("Curriculum Built " + curriculumFile.replace(".js", ""));
+        resolve();
+      });
+    })
+  ).then(() => console.log("All curricula built"));
+}
+buildCurricula()
+  .catch(console.log)
+  .then(() => {
+    if (process.env.NODE_ENV === "development") {
+      devServer(PATH_BUILDS);
+    }
+  });
