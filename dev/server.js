824e5fc7498d4bb6b12bfbb5e668e5e7dfdcdcd6
alves45 <alvesdelima.lucas45@gmail.com>
2022-07-03 23:04:12 -0300

    - Modificado função de servidor, para entregar o arquivo conforme path na url da requisição;
     - Mantido função que incrementa arquivos html para autorefresh em casos de atualização dos mesmos;
    
    - Incluido possibilidade de servir arquivos pdf;
    
     - Adicionado tratamento simples de exceções apenas servindo como texto o erro pela requisição.

--- dev/server.js
+++ dev/server.js
@@ -1,52 +1,68 @@
 import http from "http";
+import fs from "fs";
+import path from "path";
 import { JSDOM } from "jsdom";
 const PORT = process.env.PORT || 3000;
 
-/**
- *
- * @param {Document} document
- * @return {void}
- */
-
-export default function (document) {
-  let newDocument = new JSDOM(document.documentElement.innerHTML).window
-    .document;
-  let devReloadScript = newDocument.createElement("script");
-  devReloadScript.innerHTML =
-    "(" +
-    (() => {
-      let toggle = false;
-      let controller = new AbortController();
-      let signal = controller.signal;
-      setInterval(() => {
-        var timeout = setTimeout(() => {
-          controller.abort();
-        }, 200);
-        fetch("/waitRes", { signal })
-          .then(() => {
-            clearInterval(timeout);
-            if (toggle) {
-              location.reload();
-            } else {
-            }
-          })
-          .catch(() => {
-            clearInterval(timeout);
-            controller = new AbortController();
-            signal = controller.signal;
-            toggle = true;
-          });
-      }, 750);
-    }).toString() +
-    ")()";
-  newDocument.head.appendChild(devReloadScript);
-  const newDocumentHtml = newDocument.documentElement.innerHTML;
+export default function (PATH_BUILDS) {
+  function renderNewHtml(documentHtml) {
+    let newDocument = new JSDOM(documentHtml).window.document;
+    let devReloadScript = newDocument.createElement("script");
+    devReloadScript.innerHTML =
+      "(" +
+      (() => {
+        let toggle = false;
+        let controller = new AbortController();
+        let signal = controller.signal;
+        setInterval(() => {
+          var timeout = setTimeout(() => {
+            controller.abort();
+          }, 200);
+          fetch("/waitRes", { signal })
+            .then(() => {
+              clearInterval(timeout);
+              if (toggle) {
+                location.reload();
+              } else {
+              }
+            })
+            .catch(() => {
+              clearInterval(timeout);
+              controller = new AbortController();
+              signal = controller.signal;
+              toggle = true;
+            });
+        }, 750);
+      }).toString() +
+      ")()";
+    newDocument.head.appendChild(devReloadScript);
+    return newDocument.documentElement.innerHTML;
+  }
   http
     .createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.write(newDocumentHtml);
-      res.end();
+      if (req.url === "/waitRes") {
+        res.end();
+      } else {
+        let file = "";
+        try {
+          file = fs.readFileSync(
+            path.resolve(PATH_BUILDS, req.url.replace("/", ""))
+          );
+          if (req.url.includes(".html")) {
+            file = renderNewHtml(file);
+            res.setHeader("Content-Type", "text/html");
+          } else {
+            res.setHeader("Content-Type", "application/pdf");
+          }
+          res.statusCode = 200;
+          res.write(file);
+          res.end();
+        } catch (err) {
+          res.statusCode = 404;
+          res.write(err.toString());
+          res.end();
+        }
+      }
     })
     .listen(PORT);
 }
