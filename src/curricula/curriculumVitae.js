8a5991f22c2349f738441c787561d41d4542e607
alves45 <alvesdelima.lucas45@gmail.com>
2022-07-05 22:10:55 -0300

    - Adicionado utilização do componenete image;
    - Concluído cabeçalho do currículo.

--- src/curricula/curriculumVitae.js
+++ src/curricula/curriculumVitae.js
@@ -1,43 +1,87 @@
-import { css } from "@emotion/css";
+import { css, flush } from "@emotion/css";
+import { consts } from "../tools/css.js";
 import curriculumClass from "../tools/curriculumClass.js";
+import path from "path";
+import _image from "../components/image.js";
 
 export default class extends curriculumClass {
   render() {
     let document = this.document;
+    let body = this.document.body;
     let style = this.style;
+    const addStyle = this.addStyle.bind(this);
     const colors = {
       primary: "rgb(51, 83, 132)",
+      secondary: "rgb(91, 167, 209)",
     };
-    style.addG(
+    const cssUncolorizedSide = css({
+      padding: consts.s4,
+      justifyContent: "right",
+      flex: 1,
+    });
+    const cssColorizedSide = css({
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: consts.s62,
+      backgroundColor: colors.primary,
+    });
+    const image = new _image(document).render;
+    const imageElement = image(path.resolve("src/otherFiles/profileImage.JPG"));
+    this.addStyle(
+      imageElement,
       css({
-        html: {
-          boxSizing: "border-box",
-          backgroundColor: "black",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        },
-        body: {
-          margin: 0,
-          width: "210mm",
-          height: "297mm",
-          backgroundColor: "white",
-        },
-        div: {
-          width: "36%",
-          height: "100%",
-          backgroundColor: colors.primary,
-        },
-        h1: {
-          color: "red",
-        },
+        height: consts.s32,
+        width: consts.s32,
+        borderRadius: "100%",
       })
     );
-    let div = document.createElement("div");
-    let h1 = document.createElement("h1");
-    h1.innerHTML = "CURRICULARIZAÇÃO";
-    div.appendChild(h1);
-    document.body.appendChild(div);
+    const headerColorized = document.createElement("div");
+    headerColorized.append(imageElement);
+    this.addStyle(headerColorized, cssColorizedSide);
+    const headerUncolorized = document.createElement("div");
+    this.addStyle(headerUncolorized, cssUncolorizedSide);
+    const name = document.createElement("h1");
+    name.innerHTML = "Lucas Alves De Lima";
+    addStyle(
+      name,
+      css({
+        color: colors.primary,
+        textTransform: "uppercase",
+      })
+    );
+    const ornament = document.createElement("div");
+    addStyle(
+      ornament,
+      css({
+        height: consts.s0_75,
+        width: consts.s20,
+        marginRight: "auto",
+        backgroundColor: "black",
+        backgroundColor: colors.primary,
+      })
+    );
+    const workType = document.createElement("h3");
+    workType.innerHTML = "Desenvolvedor fullStack";
+    addStyle(
+      workType,
+      css({
+        color: colors.secondary,
+        textTransform: "uppercase",
+      })
+    );
+    headerUncolorized.append(name, ornament, workType);
+    const header = document.createElement("div");
+    addStyle(
+      header,
+      css({
+        display: "flex",
+        flexDirection: "row",
+      })
+    );
+    header.append(headerColorized, headerUncolorized);
+
+    body.append(header);
     return super.render();
   }
 }
