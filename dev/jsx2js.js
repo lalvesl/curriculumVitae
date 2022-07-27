e3b447ae76faff0aea9781c403e066695d74e188
alves45 <alvesdelima.lucas45@gmail.com>
2022-07-27 01:12:03 -0300

    Criado função que separa por tipos de strings de maneira recursiva, servirá para separar de maneira mais fácil e precisa códigos html e js mesmo com suas particularidades.

--- dev/jsx2js.js
+++ dev/jsx2js.js
@@ -1,4 +1,4 @@
-const js = `import logo from "./logo.svg";
+let js = `import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,15 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p123>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p123>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React function name(params) {}
           asd asd asd asd adsdasdasd asd asd
         </a>
@@ -38,11 +37,10 @@ function App2() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-l\\"ink"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <b>Learn React</b>
         </a>
         function name(params) {}
@@ -60,7 +58,62 @@ function regex2Index(regexs) {
   });
 }
 
+class blob {
+  constructor(start, txt) {
+    this.start = start;
+    this.txt = txt;
+  }
+  get length() {
+    return this.txt.length;
+  }
+}
+
+function schOffset(txt, pattern, offset) {
+  offset = offset || 0;
+  let index = 0;
+  while (true) {
+    index = txt.slice(offset).search(pattern);
+    if (txt[index - 1] !== "\\") break;
+    offset = index + 1;
+  }
+  return index + offset;
+}
+
+function findBlobs(txt = "", startPattern, endPattern) {
+  endPattern = endPattern || startPattern;
+  let start = txt.search(startPattern);
+  if (start == -1) return [];
+  let end = schOffset(txt, endPattern, start + 1);
+  if (end == -1)
+    throw new Error(
+      'syntax with error in "' +
+        startPattern +
+        '" to "' +
+        endPattern +
+        '" caracter start/end at ' +
+        start +
+        ' in "' +
+        txt.toString() +
+        '"'
+    );
+  let newStart = schOffset(txt, startPattern, start + 1);
+  const middles =
+    end < newStart
+      ? findBlobs(txt.slice(start + 1), startPattern, endPattern)
+      : [];
+  return [new blob(start, txt.slice(start, end + 1))].concat(
+    middles,
+    findBlobs(txt.slice(end + 1), startPattern, endPattern)
+  );
+}
+
 export default function jsx2js() {
+  const metaTexts = [/"/, /'/, /`/].reduce((old, current) => {
+    return old.concat(findBlobs(js, current)).map((el) => {
+      js = js.replace(el.txt, "");
+      return el;
+    });
+  }, []);
   let starts = regex2Index(js.matchAll(/<(?=\w+)/g));
   let ends = regex2Index(js.matchAll(/(?<=\/(\w*))>/g));
   let htmlIndexes = starts.map((start, i) => {
@@ -78,17 +131,21 @@ export default function jsx2js() {
     [htmlIndexes[0]]
   );
   htmlObjs.forEach((htmlObj) => {
-    function findEndBlob(txt, specialCaracter) {}
     let newCode = "";
-    const textHtml = js.slice(htmlObj.start, htmlObj.end);
+    let textHtml = js.slice(htmlObj.start, htmlObj.end);
     const el = textHtml.match(/\w+/)[0]; //document.createElement(textHtml.match(/\w+/));
     newCode += "(()=>{let el document.createElement(" + el + ");\n";
     newCode += "";
-    let textWithoutUncertainties = textHtml.replace(
-      /((\"[^\"]*\")|(\'[^\']*\')|(\`[^\`]*\`))*")/gm,
-      ""
-    );
-    textWithoutUncertainties.match(/[^\{\}]+|\{(?R)*\}/gm).map((match) => {});
+    // console.log(
+    // findBlobs(textHtml, '"').forEach(
+    //   (el) => (textHtml = textHtml.replace(el.txt, ""))
+    // );
+    console.log(textHtml);
+    // let textWithoutUncertainties = textHtml.replace(
+    //   /((\"[^\"]*\")|(\'[^\']*\')|(\`[^\`]*\`))*/gm,
+    //   ""
+    // );
+    // textWithoutUncertainties.match(/[^\{\}]+|\{(?R)*\}/gm).map((match) => {});
     // textHtml.split(/\{.*\}/g);
     // console.log(textHtml.split(/(?!=\\)(?=\{)|(?<=[^\\]\})/g));
     newCode += "})()";
