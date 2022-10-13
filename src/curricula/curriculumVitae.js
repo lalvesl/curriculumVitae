91291efd1a559b33ce07ad5ca827d2efe8ec1895
alves45 <alvesdelima.lucas45@gmail.com>
2022-10-13 00:42:56 -0300

    Alterado para o novo padrão de componentes.

--- src/curricula/curriculumVitae.js
+++ src/curricula/curriculumVitae.js
@@ -2,6 +2,8 @@ import { css } from "@emotion/css";
 import { consts } from "../tools/css.js";
 import curriculumClass from "../tools/curriculumClass.js";
 import { image } from "../components/image.js";
+import _icon from "../components/icon.js";
+import _contacts from "../components/contacts.js";
 
 export default class extends curriculumClass {
   colors = {
@@ -9,6 +11,8 @@ export default class extends curriculumClass {
     secondary: "rgb(91, 167, 209)",
   };
   render() {
+    const icon = _icon(this);
+    const contacts = _contacts(this);
     const imageElement = image("src/mediaFiles/profileImage.JPG");
     imageElement.classList.add(
       css({
@@ -17,46 +21,92 @@ export default class extends curriculumClass {
         borderRadius: "100%",
       })
     );
-    const app = (
-      <app>
-        <header
-          class={css({
-            display: "flex",
-            flexDirection: "row",
-          })}
-        >
-          {this.colorizedSide(imageElement) +
-            this.uncolorizedSide(
-              <h1
-                class={css({
-                  color: this.colors.primary,
-                  textTransform: "uppercase",
-                })}
-              >
-                Lucas Alves De Lima
-              </h1>,
-              <div
-                class={css({
-                  height: consts.s0_75,
-                  width: consts.s20,
-                  marginRight: "auto",
-                  backgroundColor: "black",
-                  backgroundColor: this.colors.primary,
-                })}
-              ></div>,
-              <h3
-                class={css({
-                  color: this.colors.secondary,
-                  textTransform: "uppercase",
-                })}
-              >
-                Desenvolvedor Fullstack
-              </h3>
-            )}
-        </header>
-      </app>
+    const header = (
+      <header
+        class={css({
+          display: "flex",
+          flexDirection: "row",
+        })}
+      >
+        {this.colorizedSide(imageElement) +
+          this.uncolorizedSide(
+            <h1
+              class={css({
+                color: this.colors.primary,
+                textTransform: "uppercase",
+              })}
+            >
+              Lucas Alves De Lima
+            </h1>,
+            <div
+              class={css({
+                height: consts.s0_75,
+                width: consts.s20,
+                marginRight: "auto",
+                backgroundColor: "black",
+                backgroundColor: this.colors.primary,
+              })}
+            ></div>,
+            <h3
+              class={css({
+                color: this.colors.secondary,
+                textTransform: "uppercase",
+              })}
+            >
+              Desenvolvedor Fullstack
+            </h3>
+          )}
+      </header>
     );
-    this.document.body.append(app);
+    const smallText = css({
+      fontWeight: 100,
+      fontSize: consts.s4,
+      letterSpacing: "0.001rem",
+      lineBreak: "strict",
+    });
+    const section = (
+      <section>
+        {this.colorizedSide(
+          <div class={css({ color: "white" })}>
+            <div
+              class={css({
+                marginLeft: consts.s2,
+              })}
+            >
+              <h5
+                class={css({
+                  textTransform: "uppercase",
+                  textAlign: "left",
+                })}
+              >
+                Contato
+              </h5>
+              <div>
+                {[
+                  contacts("person_pin_circle", "Jaraguá do Sul-SC, 89253-830"),
+                  contacts(
+                    "mail",
+                    "alvesdelima.lucas45@gmail.com",
+                    "mailto:alvesdelima.lucas45@gmail.com"
+                  ),
+                  contacts(
+                    "web",
+                    "alves45.herokuapp.com",
+                    "https://alves45.herokuapp.com/"
+                  ),
+                  contacts(
+                    "linkedIn",
+                    "alves-lucas",
+                    "https://www.linkedin.com/in/alves-lucas-834100215"
+                  ),
+                ]}
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
+    );
+    this.document.body.append(header, section);
     return super.render();
   }
   colorizedSide(...subElements) {
