5763c84fb6f45a59519e7ecd6c4c4b826fb1da56
alves45 <alvesdelima.lucas45@gmail.com>
2022-10-16 23:21:17 -0300

    Movido função de adição de css e scripts a serem expostos para o frontend para um método da classe curriculum e adicionado funcionalidades como recursão de componentes.

--- src/tools/curriculumClass.js
+++ src/tools/curriculumClass.js
@@ -1,8 +1,9 @@
-import { cache, css } from "@emotion/css";
+import { css } from "@emotion/css";
 import { JSDOM } from "jsdom";
 import { promises as fs } from "fs";
-import { consts, _style, iconInit } from "./css.js";
+import { consts, _style } from "./css.js";
 import bin64 from "../../utils/bin64.js";
+import { scriptExposer } from "./exposerScript.js";
 // const data = bin64.to(
 //   new Uint8Array(await fs.readFile("./src/mediaFiles/icons.woff2"))
 // );
@@ -26,43 +27,74 @@ const whitePage = await fs
 export default class {
   constructor() {
     this.style = new _style();
+    this.script = new scriptExposer();
     this.window = new JSDOM(whitePage).window;
     this.document = this.window.document;
+  }
+  addComponent(...components) {
+    components.flat().forEach((component) => {
+      if (component !== undefined) {
+        let props = Object.assign(
+          {
+            styles: [],
+            gStyles: [],
+            funcs: [],
+            runners: [],
+          },
+          component.props
+        );
+        this.addComponent(component.subComponents);
+        this.style.add(props.styles);
+        this.style.addG(props.gStyles);
+        {
+          [props.funcs]
+            .flat()
+            .forEach((func) => this.script.func(func, props.replacer));
+        }
+        [props.runners]
+          .flat()
+          .forEach((any) => this.script.runner(any, props.replacer));
+        // if (props.expose)
+        //   this.script.runners({
+        //     [`${props.name}`]: js2str(props.component, props.replacer),
+        //   });
+      }
+    });
   }
   colors = {
     primary: "black",
     secondary: "white",
   };
   render() {
-    let styleElement = this.document.createElement("style");
-    styleElement.innerHTML = this.style.buildStyles(
-      this.document.documentElement,
-      css({
-        ":root": {
-          fontSize: "4,5mm",
-          fontFamily: `Noto Sans`,
-          fontFeatureSettings:
-            '"kern" 0,"calt" 0,"liga" 0,"clig" 0,"dlig" 0,"hlig" 0',
-          fontWeight: 700,
-          letterSpacing: consts.s0_75,
-        },
-        html: {
-          boxSizing: "border-box",
-          backgroundColor: "black",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        },
-        body: {
-          margin: 0,
-          width: "210mm",
-          height: "297mm",
-          backgroundColor: "white",
-        },
-      }),
-      iconInit
+    this.document.head.append(
+      this.script.build(),
+      this.style.buildStyles(
+        this.document.documentElement,
+        css({
+          ":root": {
+            fontSize: "4,5mm",
+            fontFamily: `Noto Sans`,
+            fontFeatureSettings:
+              '"kern" 0,"calt" 0,"liga" 0,"clig" 0,"dlig" 0,"hlig" 0',
+            fontWeight: 700,
+            letterSpacing: consts.s0_75,
+          },
+          html: {
+            boxSizing: "border-box",
+            backgroundColor: "black",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          },
+          body: {
+            margin: 0,
+            width: "210mm",
+            height: "297mm",
+            backgroundColor: "white",
+          },
+        })
+      )
     );
-    this.document.head.appendChild(styleElement);
     return this.document.documentElement.outerHTML;
   }
   /**
